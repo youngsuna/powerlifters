ee0c8bb662097190ea36ef9c23aba8e28a3f1655
youngsuna <youngsunas2@gmail.com>
2024-08-30 18:03:25 +0900

    initial commot

---
+++ powerlifters/src/main/java/com/appleground/powerlifters/PowerliftersApplication.java
@@ -0,0 +1,13 @@
+package com.appleground.powerlifters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerliftersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerliftersApplication.class, args);
+	}
+
+}

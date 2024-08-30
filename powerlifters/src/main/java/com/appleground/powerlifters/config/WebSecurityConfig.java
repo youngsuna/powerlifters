dd4ad28c1acb92b70499c1ff482ad6556b7ee196
youngsuna <youngsunas2@gmail.com>
2024-08-30 20:09:52 +0900

    9. 12:50

---
+++ powerlifters/src/main/java/com/appleground/powerlifters/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.appleground.powerlifters.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.appleground.powerlifters.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configurable
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws exception {
+        // httpSecurity
+        //     .cor
+    }
+
+}
---
+++ powerlifters/src/main/java/com/appleground/powerlifters/provider/JwtProvider.java
@@ -0,0 +1,44 @@
+package com.appleground.powerlifters.provider;
+
+import java.util.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtProvider {
+    
+    private String secretkey = "S3cr3tk3y";
+
+    public String create(String email) {
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+
+        String jwt = Jwts.builder()
+            .signWith (SignatureAlgorithm.ES256, secretkey)
+            .setSubject(email).setIssuedAt (new Date()).setExpiration(expiredDate)
+            .compact();
+
+        return jwt;
+    }
+
+    public String validate (String jwt) {
+        
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parser().setSigningKey(secretkey)
+                .parseClaimsJws(jwt).getBody();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+        return claims.getSubject();
+    }
+
+
+}

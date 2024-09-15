a81dd356015ccbaa4044e1c1ac98bc49ebb83755
youngsuna <youngsunas2@gmail.com>
2024-09-16 00:05:02 +0900

    26. Auth 페이지 공통 스타일 작성

--- front/src/views/Authentication/index.tsx
+++ front/src/views/Authentication/index.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 //      component: 인증 화면 컴포넌트       //
 export default function Authentication() {
+
+  //    state: 화면 상태    //
+  const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
+
+  
+  //      component: sign in card 컴포넌트       //
+  const SignInCard = () => {
     
     //      render: 인증 화면 컴포넌트 렌더링       //
-    return (
-    <div>
-      인증화면
+    return(
+      <div className='auth-card'></div>
+    );
+  };
+
+    //      component: sign up card 컴포넌트       //
+    const SignUpCard = () => {
+      
+      //      render: 인증 화면 컴포넌트 렌더링       //
+      return(
+        <div className='auth-card'></div>
+      );
+    };
+
+  //      render: 인증 화면 컴포넌트 렌더링       //
+  return (
+    <div id='auth-wrapper'>
+      <div className='auth-container'>
+        <div className='auth-jumbotron-box'>
+          <div className='auth-jumbotron-contents'>
+          <div className='auth-logo-icon'></div>
+            <div className='auth-jumbotron-text-box'>
+              <div className='auth-jumbotron-text'>{'환영합니다.'}</div>
+              <div className='auth-jumbotron-text'>{'PowerLifters 입니다.'}</div>
+            </div>
+          </div>
+        </div>
+        {view === 'sign-in' && <SignInCard />}
+        {view === 'sign-up' && <SignUpCard />}
+      </div>
     </div>
   )
 }

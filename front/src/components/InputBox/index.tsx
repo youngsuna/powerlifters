1b66e5ddc0d490aeb9689a9c5d02fd62601e486b
youngsuna <youngsunas2@gmail.com>
2024-09-16 12:18:23 +0900

    27. Auth 페이지 로그인 카드 A

--- front/src/components/InputBox/index.tsx
+++ front/src/components/InputBox/index.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, Dispatch, forwardRef, SetStateAction, KeyboardEvent } from 'react'
 import './style.css'
 
+//      interface: Input Box 컴포넌터 Propperties       //
 interface Props {
     label: string;
     type: 'text' | 'password';
@@ -9,7 +10,7 @@ interface Props {
     value: string;
     setValue: Dispatch<SetStateAction<string>>;
    
-    icon?: string;
+    icon?: 'eye-light-off-icon' | 'eye-light-on-icon' | 'expand-right-light-icon';
     onButtonClick?: () => void;
 
     message?: string;
@@ -41,7 +42,7 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
             <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
                 <input ref={ref} type={type} className='input' placeholder = {placeholder} value = {value} onChange={onChangeHandler} onKeyDown={onKeyDownHandler}/>
                 {onButtonClick !== undefined && (
-                    <div className='icon-button'>
+                    <div className='icon-button' onClick={onButtonClick}>
                         {icon !== undefined && <div className={`icon ${icon}`}></div>}    
                     </div>
                 )}
